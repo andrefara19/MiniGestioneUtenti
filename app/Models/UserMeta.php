c98f9fd8a5b02749f05ef692eacee8c11b2f1689
andrefara19 <effarao@gmail.com>
2024-07-20 12:36:49 +0200

    Model UserMeta

---
+++ app/Models/UserMeta.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserMeta extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'nome', 'cognome', 'indirizzo', 'cap', 'citta', 'provincia', 'nazione_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function nazione()
+    {
+        return $this->belongsTo(Country::class, 'nazione_id');
+    }
+}
