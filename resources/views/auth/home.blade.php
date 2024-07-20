6f2a829537ba6f71b8462fb602b6cd48b8d84549
andrefara19 <effarao@gmail.com>
2024-07-20 20:42:16 +0200

    Costruzione pagina Home.blade.php

--- resources/views/auth/home.blade.php
+++ resources/views/auth/home.blade.php
@@ -1,13 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $nome }} {{ $cognome }}</title>
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/form.css') }}" rel="stylesheet">
 </head>
 <body>
-    <h1>Benvenuto nella tua area personale, {{ $nome }} {{ $cognome }}</h1>
-    <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <button type="submit">Logout</button>
-    </form>
+    <header class="header">
+        <span class="logo">Mini Gestione Utenti</span>
+        <input id="menu-button" class="menu-button" type="checkbox">
+        <label for="menu-button" class="menu-label">
+            <span class="menu-icon"></span>
+        </label>
+        <ul class="menu">
+            <li><a href="{{ url('/registrati/') }}">REGISTRATI</a></li>
+            <li><a href="{{ url('/login/') }}">LOGIN</a></li>
+        </ul>
+    </header>
+    <main class="main">
+        <h1>Benvenuto nella tua area personale, {{ $nome }}!</h1>
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit">Logout</button>
+        </form>
+    </main>
 </body>
 </html>
