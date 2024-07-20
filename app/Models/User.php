c35e64972aed297000c3dd4573d36feef9a79baf
andrefara19 <effarao@gmail.com>
2024-07-20 18:38:44 +0200

    Modifica di user.php per is_admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $fillable = ['email', 'password'];
+    protected $fillable = ['email', 'password', 'is_admin'];
 
     protected $hidden = ['password', 'remember_token'];
 
