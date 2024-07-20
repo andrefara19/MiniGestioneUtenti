9b87174de655aee0123af3c3514df22160945a55
andrefara19 <effarao@gmail.com>
2024-07-20 19:31:24 +0200

    Correzioni e aggiornamenti login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/form.css') }}" rel="stylesheet">
 </head>
 <body>
-    @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="{{ route('login') }}" method="POST">
-        @csrf
-        <input type="email" name="email" placeholder="Email" required value="{{ old('email') }}">
-        <input type="password" name="password" placeholder="Password" required minlength="8">
-        <button type="submit">Login</button>
-    </form>
+    <header class="header">
+        <span class="logo">Mini Gestione Utenti</span>
+        <input id="menu-button" class="menu-button" type="checkbox">
+        <label for="menu-button" class="menu-label">
+            <span class="menu-icon"></span>
+        </label>
+        <ul class="menu">
+            <li><a href="{{ url('/registrati/') }}">REGISTRATI</a></li>
+            <li><a href="{{ url('/login/') }}">LOGIN</a></li>
+        </ul>
+    </header>
+    <main class="main">
+        @if ($errors->any())
+            <div>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <input type="email" name="email" placeholder="Email" required value="{{ old('email') }}">
+            <br><br>
+            <input type="password" name="password" placeholder="Password" required minlength="8">
+            <br><br>
+            <button type="submit">Login</button>
+        </form>
+    </main>
 </body>
 </html>
 
