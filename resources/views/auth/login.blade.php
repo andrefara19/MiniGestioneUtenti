036bcc43a4c2f7cacdc23929df0f865b4e475cc7
andrefara19 <effarao@gmail.com>
2024-07-20 19:06:23 +0200

    login.blade.php aggiornato per assolvere alla sua funzione

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,25 @@
 <!DOCTYPE html>
-<html lang="it">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrati</title>
-    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/form.css') }}" rel="stylesheet">
+    <title>Login</title>
 </head>
-
-<body class="body">
-    <header class="header">
-        <span class="logo">Mini Gestione Utenti</span>
-        <input id="menu-button" class="menu-button" type="checkbox">
-        <label for="menu-button" class="menu-label">
-            <span class="menu-icon"></span>
-        </label>
-        <ul class="menu">
-        <li><a href="{{ url('/registrati/') }}">REGISTRATI</a></li>
-        <li><a href="{{ url('/login/') }}">LOGIN</a></li>
-        </ul>
-    </header>
-    <main class="main">
-        <form action="{{ route('login') }}" method="POST">
-            @csrf
-            <input type="email" name="email" placeholder="Email" required>
-            <br><br>
-            <input type="password" name="password" placeholder="Password" required>
-            <br><br>
-            <button type="submit">Login</button>
-        </form> 
-    </main>
+<body>
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+        <input type="email" name="email" placeholder="Email" required value="{{ old('email') }}">
+        <input type="password" name="password" placeholder="Password" required minlength="8">
+        <button type="submit">Login</button>
+    </form>
 </body>
+</html>
 
 </html>
