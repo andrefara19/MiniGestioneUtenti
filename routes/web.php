6b211f3465681992e8b74ac8159d6bb628082c4e
andrefara19 <effarao@gmail.com>
2024-07-20 23:58:03 +0200

    Visualizzazione nazione e profilo--> home

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\HomeController;
+use App\Http\Controllers\Auth\ProfileController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('registrati', [RegisterController::class, 'showRegistrationForm'])->name('register');
@@ -14,4 +16,5 @@ Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
     Route::get('/area_personale', [HomeController::class, 'index'])->name('home');
+    Route::get('/profilo', [ProfileController::class, 'index'])->name('profile');
 });
