037fc641fd9e430c20469ff5a4e70046fe52c1b3
andrefara19 <effarao@gmail.com>
2024-07-20 13:36:05 +0200

    Modifiche web.php

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('registrati', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('registrati', [RegisterController::class, 'register']);
 
-Route::get('/registrati', function () {
-    return view('registrati');
-})->name('registrati');
-
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
