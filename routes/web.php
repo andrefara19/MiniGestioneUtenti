ee4c330e1501906fc64a12fabaa7c817b2934bb3
andrefara19 <effarao@gmail.com>
2024-07-20 19:23:17 +0200

    Correzioni e aggiornamenti web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('registrati', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('registrati', [RegisterController::class, 'register']);
+
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
