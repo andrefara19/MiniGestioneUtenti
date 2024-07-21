fdd1d6cf2e23c1131829905e209b43a6d73edbfb
andrefara19 <effarao@gmail.com>
2024-07-21 18:05:48 +0200

    Ripristino pagina iniziale Laravel

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Auth\HomeController;
 use App\Http\Controllers\Auth\ProfileController;
 use App\Http\Controllers\Auth\UserController;
 
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
