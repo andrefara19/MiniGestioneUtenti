f65926ba7626ddc51a81c21557d416298b6c49cd
andrefara19 <effarao@gmail.com>
2024-07-21 13:16:24 +0200

    Aggiornamento per visualizzazione lista link utenti

--- app/Http/Controllers/Auth/HomeController.php
+++ app/Http/Controllers/Auth/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\UserMeta;
 
 class HomeController extends Controller
 {
@@ -12,10 +14,12 @@ class HomeController extends Controller
     {
         $user = Auth::user();
         $userMeta = $user->userMeta;
+        $users = User::with('userMeta')->get();
 
         return view('auth.home', [
             'nome' => $userMeta->nome,
             'cognome' => $userMeta->cognome,
+            'users' => $users,
         ]);
     }
 }
