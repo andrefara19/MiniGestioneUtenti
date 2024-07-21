754039761b5ba2340cfb181f7ae8d4370d116ad0
andrefara19 <effarao@gmail.com>
2024-07-21 11:50:11 +0200

    Aggiornamento per modifiche dal profilo

--- app/Http/Controllers/Auth/ProfileController.php
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Country;
 
 class ProfileController extends Controller
 {
@@ -11,6 +13,7 @@ class ProfileController extends Controller
     {
         $user = Auth::user();
         $userMeta = $user->userMeta;
+        $countries = Country::all(); 
         $country = $userMeta->country; 
 
         return view('auth.profile', [
@@ -20,7 +23,37 @@ class ProfileController extends Controller
             'cap' => $userMeta->cap,
             'citta' => $userMeta->citta,
             'provincia' => $userMeta->provincia,
+            'nazione_id' => $userMeta->nazione_id,
             'nazione' => $country ? $country->name : 'N/A',
+            'countries' => $countries, // Passa la lista dei paesi alla vista
         ]);
+    }
+
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+        $userMeta = $user->userMeta;
+
+        $request->validate([
+            'nome' => 'nullable|string|max:255',
+            'cognome' => 'nullable|string|max:255',
+            'indirizzo' => 'nullable|string|max:255',
+            'cap' => 'nullable|string|max:20',
+            'citta' => 'nullable|string|max:255',
+            'provincia' => 'nullable|string|max:255',
+            'nazione_id' => 'nullable|exists:countries,id',
+        ]);
+
+        $userMeta->update([
+            'nome' => $request->nome,
+            'cognome' => $request->cognome,
+            'indirizzo' => $request->indirizzo,
+            'cap' => $request->cap,
+            'citta' => $request->citta,
+            'provincia' => $request->provincia,
+            'nazione_id' => $request->nazione_id,
+        ]);
+
+        return redirect()->route('profile.index')->with('success', 'Profilo aggiornato con successo!');
     }
 }
