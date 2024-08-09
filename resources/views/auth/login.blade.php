48b801dc98e49c48d97c6a55eb58ffd98fce2e8a
andrea <alessandro.paganelli@tuscanyleather.it>
2024-08-09 11:33:30 +0200

    Fattorizzato codice login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
-</head>
-<body>
-    <header class="header">
-        <span class="logo">Mini Gestione Utenti</span>
-        <input id="menu-button" class="menu-button" type="checkbox">
-        <label for="menu-button" class="menu-label">
-            <span class="menu-icon"></span>
-        </label>
-        <ul class="menu">
-            <li><a href="{{ url('/registrati/') }}">REGISTRATI</a></li>
-            <li><a href="{{ url('/login/') }}">LOGIN</a></li>
-        </ul>
-    </header>
-    <main class="main">
+@extends('layouts.app')
+
+@section('title', 'Registrati')
+
+@section('extra-css')
+<link href="{{ asset('css/login.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
         @if ($errors->any())
             @foreach ($errors->all() as $error)
                 <div class = "fail-message">
@@ -35,8 +22,4 @@
             <br><br>
             <button class="login_button" type="submit">Login</button>
         </form>
-    </main>
-</body>
-</html>
-
-</html>
+@endsection
