874d6e013a7422b70f06a01d1dda8a7af0fbb23e
andrea <alessandro.paganelli@tuscanyleather.it>
2024-08-09 13:02:45 +0200

    app.blade.php rinominato in guest.blade.php e funzionante

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Default Title')</title>
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+    @yield('extra-css')
+</head>
+
+<body class="body">
+    <header class="header">
+        <span class="logo">Mini Gestione Utenti</span>
+        <input id="menu-button" class="menu-button" type="checkbox">
+        <label for="menu-button" class="menu-label">
+            <span class="menu-icon"></span>
+        </label>
+        <ul class="menu">
+            @guest
+                <li><a href="{{ url('/registrati/') }}">REGISTRATI</a></li>
+                <li><a href="{{ url('/login/') }}">LOGIN</a></li>
+            @else
+                <li><a href="{{ url('/profilo/') }}">PROFILO</a></li>
+                <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">LOGOUT</a></li>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            @endguest
+        </ul>
+    </header>
+
+    <main class="main">
+        @yield('content')
+    </main>
+
+    @yield('extra-scripts')
+</body>
+
+</html>
