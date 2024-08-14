ae0e6ea0b9c7874aa096d2097bf2e71bbcd28eb4
andrea <andrea.faraone@rossotech.it>
2024-08-14 12:03:40 +0200

    Ombreggiatura h1 h2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Homepage')
 
 @section('content')
-        <h2 style="text-align:center; text-shadow: 2px 2px 2px black;">Welcome in<h2>
-        <h1 style="text-align:center; margin-top:30px; font-size: 50px; text-shadow: 2px 2px 2px black;">MiniGestioneUtenti<h1>
+        <h2>Welcome in<h2>
+        <h1 style="margin-top:30px; font-size: 50px;">MiniGestioneUtenti<h1>
 @endsection
 
 
