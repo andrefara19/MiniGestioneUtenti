e868383835512f2f7ed1c3f28ef7eb0cf793093e
andrea <andrea.faraone@rossotech.it>
2024-08-20 15:18:54 +0200

    RegisterRequest

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\UserMeta;
 use App\Models\Country;
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -23,75 +23,8 @@ class RegisterController extends Controller
         return response()->json($countries);
     }
 
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        $messages = [
-            'nome.regex_start' => 'Il nome non può iniziare con spazio',
-            'nome.regex' => 'Nel nome sono presenti numeri o caratteri speciali',
-            'nome.regex_space' => 'Nel nome, dopo lo spazio, serve un carattere',
-            'nome.required' => 'Il nome non può essere vuoto',
-
-            'cognome.regex_start' => 'Il cognome non può iniziare con spazio',
-            'cognome.regex' => 'Nel cognome sono presenti numeri o caratteri speciali',
-            'cognome.regex_space' => 'Nel cognome, dopo lo spazio, serve un carattere',
-            'cognome.required' => 'Il cognome non può essere vuoto',
-
-            'password.min' => 'La password deve contenere almeno 8 caratteri',
-            'password.required' => 'La password non può essere vuota',
-
-            'email.email' => 'Immettere una email valida',
-            'email.regex' => 'Immettere una email valida',
-            'email.unique' => 'L\'email è già stata utilizzata',
-            'email.required' => 'L\'email non può essere vuota',
-
-            'cellulare.unique' => 'Il numero di cellulare è già stato utilizzato',
-            'cellulare.required' => 'Il cellulare non può essere vuoto',
-            'cellulare.max' => 'Il numero di cellulare deve essere di 10 cifre',
-            'cellulare.min' => 'Il numero di cellulare deve essere di 10 cifre',
-            'cellulare.regex' => 'Il numero di cellulare deve contenere solo cifre',
-
-            'nazione_id.required' => 'La nazione deve essere selezionata'
-        ];
-
-        $validator = Validator::make($request->all(), [
-            'nome' => [
-                'required',
-                'string',
-                'max:255',
-                'regex:/^[^\s][A-Za-zÀ-ÿ\s]*[^\s]$/',
-                'regex:/^(?!.*[0-9!@#\$%\^&\*\(\)_\+={}\[\]\|\\:;\"\'<>,\.\?\/~`]).*$/'
-            ],
-            'cognome' => [
-                'required',
-                'string',
-                'max:255',
-                'regex:/^[^\s][A-Za-zÀ-ÿ\s]*[^\s]$/',
-                'regex:/^(?!.*[0-9!@#\$%\^&\*\(\)_\+={}\[\]\|\\:;\"\'<>,\.\?\/~`]).*$/'
-            ],
-            'cellulare' => [
-                'required',
-                'string',
-                'max:10',
-                'min:10',
-                'regex:/^[0-9]{10}$/',
-                'unique:user_meta'
-            ],
-            'indirizzo' => 'nullable|string|max:255',
-            'cap' => 'nullable|string|max:20',
-            'citta' => 'nullable|string|max:255',
-            'provincia' => 'nullable|string|max:255',
-            'nazione_id' => 'required|exists:countries,id',
-            'email' => 'required|string|email|regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
-        ], $messages);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'success' => false,
-                'errors' => $validator->errors(),
-            ], 422);
-        }
-
         $user = User::create([
             'email' => $request->email,
             'password' => Hash::make($request->password),
--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -4,14 +4,12 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TryRequest extends FormRequest
+class RegisterRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +20,72 @@ class TryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+           'nome' => [
+                'required',
+                'string',
+                'max:255',
+                'regex:/^[^\s][A-Za-zÀ-ÿ\s]*[^\s]$/',
+                'regex:/^(?!.*[0-9!@#\$%\^&\*\(\)_\+={}\[\]\|\\:;\"\'<>,\.\?\/~`]).*$/'
+            ],
+            'cognome' => [
+                'required',
+                'string',
+                'max:255',
+                'regex:/^[^\s][A-Za-zÀ-ÿ\s]*[^\s]$/',
+                'regex:/^(?!.*[0-9!@#\$%\^&\*\(\)_\+={}\[\]\|\\:;\"\'<>,\.\?\/~`]).*$/'
+            ],
+            'cellulare' => [
+                'required',
+                'string',
+                'max:10',
+                'min:10',
+                'regex:/^[0-9]{10}$/',
+                'unique:user_meta'
+            ],
+            'indirizzo' => 'nullable|string|max:255',
+            'cap' => 'nullable|string|max:20',
+            'citta' => 'nullable|string|max:255',
+            'provincia' => 'nullable|string|max:255',
+            'nazione_id' => 'required|exists:countries,id',
+            'email' => 'required|string|email|regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed', 
+        ];
+    }
+
+    /**
+     * Get the custom messages for validation errors.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'nome.regex_start' => 'Il nome non può iniziare con spazio',
+            'nome.regex' => 'Nel nome sono presenti numeri o caratteri speciali',
+            'nome.regex_space' => 'Nel nome, dopo lo spazio, serve un carattere',
+            'nome.required' => 'Il nome non può essere vuoto',
+
+            'cognome.regex_start' => 'Il cognome non può iniziare con spazio',
+            'cognome.regex' => 'Nel cognome sono presenti numeri o caratteri speciali',
+            'cognome.regex_space' => 'Nel cognome, dopo lo spazio, serve un carattere',
+            'cognome.required' => 'Il cognome non può essere vuoto',
+
+            'password.min' => 'La password deve contenere almeno 8 caratteri',
+            'password.required' => 'La password non può essere vuota',
+
+            'email.email' => 'Immettere una email valida',
+            'email.regex' => 'Immettere una email valida',
+            'email.unique' => 'L\'email è già stata utilizzata',
+            'email.required' => 'L\'email non può essere vuota',
+
+            'cellulare.unique' => 'Il numero di cellulare è già stato utilizzato',
+            'cellulare.required' => 'Il cellulare non può essere vuoto',
+            'cellulare.max' => 'Il numero di cellulare deve essere di 10 cifre',
+            'cellulare.min' => 'Il numero di cellulare deve essere di 10 cifre',
+            'cellulare.regex' => 'Il numero di cellulare deve contenere solo cifre',
+
+            'nazione_id.required' => 'La nazione deve essere selezionata'
         ];
     }
 }
+
