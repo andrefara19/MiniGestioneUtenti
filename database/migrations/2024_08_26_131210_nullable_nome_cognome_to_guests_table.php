41ee8ad7dcbbe52343dc03f59715b36ea78e99d5
andrea <andrea.faraone@rossotech.it>
2024-08-26 15:13:57 +0200

    Resi nullable nome e cognome degli accompagnatori

---
+++ database/migrations/2024_08_26_131210_nullable_nome_cognome_to_guests_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('guests', function (Blueprint $table) {
+            $table->string('nome')->nullable()->change();
+            $table->string('cognome')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('guests', function (Blueprint $table) {
+            $table->string('nome')->nullable(false)->change();
+            $table->string('cognome')->nullable(false)->change();
+        });
+    }
+};
